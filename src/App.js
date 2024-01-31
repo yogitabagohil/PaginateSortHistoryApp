10b25b09ff907ea23327b53638dab99819f19567
yogitabagohil <yogitabagohil2022@gmail.com>
2024-01-31 12:05:37 +0200

    fatch the data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
 function App() {
+  const [data, setData] = useState([]);
+
+   // Simulated API call
+   const fetchData = async () => {
+    try {
+      // Replace with actual API call
+      const response = await fetch("https://randomuser.me/api/?results=100");
+      const result = await response.json();
+console.log("result",result)
+      // Simulated response handling
+      setData(result.data);
+     
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+ 
   return (
     <div className="App">
       <div>
-        <button >Previous</button>
+        <button onClick={fetchData} >Previous</button>
         <span>{ }</span>
         <button>Next</button>
         <button >View History</button>
